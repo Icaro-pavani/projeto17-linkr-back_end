0c09be57e61d75bce5f5f6f468524d09c90c0631
Danilo <danilo_srr@hotmail.com>
2022-06-15 13:50:23 -0300

    publish route waiting for jwt on signIn

--- src/routes/publishingRouter.js
+++ src/routes/publishingRouter.js
@@ -7,6 +7,6 @@ import validToken from "../middlewares/validToken.js"
 
 const publishRouter = Router();
 
-publishRouter.post("/user/publish", validSchema(publishingSchema), validToken, publishPost);
+publishRouter.post("/user/publish", validSchema(publishingSchema), validToken, publishPost); //validToken esperando jwt ser recebido no login
 
 export default publishRouter;
