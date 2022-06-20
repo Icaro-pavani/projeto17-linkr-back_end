135b6cc62b6ea8d1e7d5e74616848c88acec4bab
Caroline LS <carolinelds@gmail.com>
2022-06-20 00:30:23 -0300

    fix: check user's token validity before requesting trending hashtags

--- src/routes/hashtagsRouter.js
+++ src/routes/hashtagsRouter.js
@@ -1,11 +1,10 @@
 import { Router } from "express";
 
 import { getTrendingHashtags } from "../controllers/hashtagsController.js";
-//import validToken from "../middlewares/validToken.js";
+import validToken from "../middlewares/validToken.js";
 
 const hashtagsRouter = Router();
 
-hashtagsRouter.get("/hashtags/trending", getTrendingHashtags);
-//hashtagsRouter.get("/hashtags/trending", validToken, getTrendingHashtags);
+hashtagsRouter.get("/hashtags/trending", validToken, getTrendingHashtags);
 
 export default hashtagsRouter;
