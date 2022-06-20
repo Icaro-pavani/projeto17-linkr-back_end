8f5493d9f7dc932c7f768caabb1d9a198a90c248
Caroline LS <carolinelds@gmail.com>
2022-06-19 22:03:51 -0300

    Configure a get endpoint to obtain trending hashtags

---
+++ src/routes/hashtagsRouter.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+import { getTrendingHashtags } from "../controllers/hashtagsController.js";
+//import validToken from "../middlewares/validToken.js";
+
+const hashtagsRouter = Router();
+
+hashtagsRouter.get("/hashtags/trending", getTrendingHashtags);
+//hashtagsRouter.get("/hashtags/trending", validToken, getTrendingHashtags);
+
+export default hashtagsRouter;
